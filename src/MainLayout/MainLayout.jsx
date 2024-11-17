136337c41dfd08e70d4f4f074eef470bb31f9c2e
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-17 21:44:23 +0600

    project setup

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div className='text-red-600 text-5xl'>
+            main layout
+        </div>
+    );
+};
+
+export default MainLayout;
