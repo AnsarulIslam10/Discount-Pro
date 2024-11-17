b7caa3ad750eff74ffc4798b6c96fa6c9b233644
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-17 22:30:18 +0600

    footer design complete

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Header from '../components/Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
+  return (
+    <div>
+      <div className="w-11/12 mx-auto max-w-[1400px]">
+        <Header></Header>
+        <div className="min-h-[calc(100vh-291.91px)]">
+          <Outlet></Outlet>
         </div>
-    );
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-[#28b5f6] text-lg font-medium text-white rounded p-10">
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Privacy Policy</a>
+        <a className="link link-hover">Terms of Service</a>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col text-4xl gap-4">
+          <a href="https://www.facebook.com/ansarulislamriyad" target="_blank" className="hover:scale-150 transition-all"><FaFacebook /></a>
+          <a href="https://github.com/AnsarulIslam10" target="_blank" className="hover:scale-150 transition-all"><FaGithub /></a>
+          <a href="https://www.linkedin.com/in/ansarul-islam-32a229318/" target="_blank" className="hover:scale-150 transition-all"><FaLinkedin /></a>
+        </div>
+      </nav>
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Ansarul Islam
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
