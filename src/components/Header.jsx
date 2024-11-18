839d833f402789ce38317130079ee791bf909e5a
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-18 20:21:46 +0600

    welcome text in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from './Navbar';
+import { AuthContext } from '../provider/AuthProvider';
 
 const Header = () => {
+    const {user} = useContext(AuthContext);
     return (
         <div>
+            {
+                user && <div className='flex flex-col justify-center items-center bg-blue-50 p-2'>
+                    <h1 className='text-lg font-medium'>Welcome, {user.displayName}.</h1>
+                    <p className='text-sm text-gray-600'>Let's grab some amazing deals today!!</p>
+                </div>
+            }
             <Navbar></Navbar>
         </div>
     );
